2c81b93c0e792cca47ea7f4c54a12529aa224dd0
Daria Kozielecka <dkozielecka@macbook-air-daria-1.home>
2019-02-13 22:43:15 +0100

    animation desrciption

---
+++ main.js
@@ -0,0 +1,21 @@
+const description = `Hej, jestem Daria. Mam 23 lata i uczę się front-end'u. Cechuje mnie
+duża upartość w dążeniu do celu i spory dystans do siebie. Lubię się
+śmiać i staram się być pozytywną osobą. W wolnych chwilach próbuję
+wszystkiego, żegluje, chodzę po linach, gram w gry komputerowe i
+planszowe, jeżdżę na nightdrivy, chodzę na siłownie, czytam książki,
+oglądam seriale i wszystko co tylko wpadnie mi do głowy. Aktualnie
+jestem na kursie "junior front-ed developer" i chętnie odbędę u kogoś
+staż.`;
+
+let descriptionContainer = document.querySelector(".about__me>p");
+let number = 0;
+let index = 0;
+
+const writing = setInterval(() => {
+  if (number < description.length) {
+    number++;
+    descriptionContainer.textContent =
+      descriptionContainer.textContent + description[index];
+    index++;
+  }
+}, 60);
