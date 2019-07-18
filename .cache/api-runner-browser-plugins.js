7f344ca5dda4ea51d3c1210456169aba047f64c6
dkozielecka <Pf3gj4dh5>
2019-07-18 14:57:54 +0200

    update files

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/dkozielecka/Front/Projekty/Portfolio/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
--- .cache/async-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-exports.components = {
-  "component---node-modules-gatsby-plugin-offline-app-shell-js": () => import("/Users/dkozielecka/Front/Projekty/Portfolio/node_modules/gatsby-plugin-offline/app-shell.js" /* webpackChunkName: "component---node-modules-gatsby-plugin-offline-app-shell-js" */),
-  "component---src-pages-404-js": () => import("/Users/dkozielecka/Front/Projekty/Portfolio/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-index-js": () => import("/Users/dkozielecka/Front/Projekty/Portfolio/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
-}
-
